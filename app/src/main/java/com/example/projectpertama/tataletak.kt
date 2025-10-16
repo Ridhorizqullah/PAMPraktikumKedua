b093b29253da5e9814f6dc138dfd7527e455e126
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-16 16:05:38 +0700

    menambhakna layout Column dengan empat komponen teks dan padding 20dp

--- app/src/main/java/com/example/projectpertama/tataletak.kt
+++ app/src/main/java/com/example/projectpertama/tataletak.kt
@@ -1,9 +1,15 @@
 package com.example.projectpertama
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @composable
 fun TataletakColumn(modifier: Modifier){
-
+    column(modifier = modifier.padding(all=20.dp)) {
+        Text(text = "Komponen 1")
+        Text(text = "Komponen 2")
+        Text(text = "Komponen 3")
+        Text(text = "Komponen 4")
+    }
 }
