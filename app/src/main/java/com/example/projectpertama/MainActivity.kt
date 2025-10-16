72f3edcf868b7e508d6701324b422ba659392e71
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-16 17:04:38 +0700

    Integrasi Scaffold dengan tema dan layout TataletakColumn

--- app/src/main/java/com/example/projectpertama/MainActivity.kt
+++ app/src/main/java/com/example/projectpertama/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectPertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TataletakColumn(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
