27b3c0b5f1abe68692ae1bef48f6551e04ca17a2
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-16 16:44:19 +0700

    Perbaikan modifier Box dengan penambahan nilai height

--- app/src/main/java/com/example/projectpertama/tataletak.kt
+++ app/src/main/java/com/example/projectpertama/tataletak.kt
@@ -1,10 +1,14 @@
 package com.example.projectpertama
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -98,5 +102,18 @@ fun TataletakRowColumn(modifier: Modifier) {
 @composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
     val gambar = painterResource(id = R.drawable.foto)
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height()
+                .background(color = Color.Green),
+            contentAlignment = Alignment.Center
+        ) {
 
+
+                )
+            }
+        }
+    }
 }
